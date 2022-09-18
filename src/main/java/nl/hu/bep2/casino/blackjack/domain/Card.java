dbb6b6cdc1b1e0ea1c2747c4d27d16daf1b03980
spook1 <6685935+spook1@users.noreply.github.com>
2022-09-18 17:48:10 +0200

    eerste klassen aangemaakt, enums en een loop in gameCards
    Loop in Gamecards opgezte die met named Constructior een  set speelkaarten aanmaakt van een opgegeven aantal decks.
    Weet niet of dit gaat werken, is met mijn roestige javakennis opgezet.

---
+++ src/main/java/nl/hu/bep2/casino/blackjack/domain/Card.java
@@ -0,0 +1,5 @@
+package nl.hu.bep2.casino.blackjack.domain;
+
+public class Card {
+
+}
---
+++ src/main/java/nl/hu/bep2/casino/blackjack/domain/GameCards.java
@@ -0,0 +1,42 @@
+package nl.hu.bep2.casino.blackjack.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GameCards {
+
+	private int numberOfDecks;
+	private List<Card> gameCards = null;
+
+
+	
+	
+	public GameCards(int numberOfDecks) {
+		this.numberOfDecks = numberOfDecks;
+	}
+	
+	public void GameCards.initateDeck(int numberOfDecks) {
+		
+		this.numberOfDecks = numberOfDecks;
+		int loop =numberOfDecks;
+		Kleur kleuren[] = Kleur.values();
+		Waarde waarden[] = Waarde.values();
+		List<Card> cards = null;
+		
+		while (loop > 0) {
+			int i=0;
+			for(Kleur kleur: kleuren) {
+				for( Waarde waarde: waarden) {
+					Card card = new Card(kleur,waarde);
+					cards.add(card);
+					i++;
+				}
+			}
+			loop--;   // loop met één verlagen als er één compleet deck is toegevoegd
+		}
+		Collections.shuffle(cards);
+		this.gameCards = cards;
+	}
+
+	
+}
