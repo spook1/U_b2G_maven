2bc2cdf5f51e5b163be81a8077943719824e2a74
spook1 <6685935+spook1@users.noreply.github.com>
2022-09-25 15:47:06 +0200

    Domain model update (new insights coour during class creation)

--- src/main/java/nl/hu/bep2/casino/test/TestController.java
+++ src/main/java/nl/hu/bep2/casino/test/TestController.java
@@ -1,8 +1,8 @@
 package nl.hu.bep2.casino.test;
 
+import java.util.HashMap;
 import java.util.List;
-
-
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +20,9 @@ public class TestController {
 	
 	private final static int numDecks =2;
 
-	@GetMapping("/test1")
+	@GetMapping("/test")
 	//public ArrayList<Card> gameDeck() {
-	public int[] testMethode() {
+	public Map<int[],Hand> testMethode() {
 	
 		Hand playerHand = new Hand(1);
 		GameCards gameCards = new GameCards(numDecks);
@@ -30,14 +30,15 @@ public class TestController {
 		int[] score = new int[2];
 		//return gameCards.getGameCards();
 		
+		Map<int[],Hand> map = new HashMap<>();
+		
 		playerHand.addCardToHand(gameCards.getCard());
 		playerHand.addCardToHand(gameCards.getCard());
-
-		 
 		 
 		score = playerHand.getHandScore();
+		map.put( score, playerHand);
 		
-		return score;
+		return map;
 		//	return gameCards.getCard();
 			
 
